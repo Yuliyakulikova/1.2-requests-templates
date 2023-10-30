654c0f4a52b981a857115edaac2ed0553e3f9c9a
Yuliyakulikova <uskulikova@yandex.ru>
2023-10-30 19:55:06 +0500

    Initial commit

---
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -0,0 +1,36 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+DATA = {
+    'omlet': {
+        'яйца, шт': 2,
+        'молоко, л': 0.1,
+        'соль, ч.л.': 0.5,
+    },
+    'pasta': {
+        'макароны, г': 0.3,
+        'сыр, г': 0.05,
+    },
+    'butter': {
+        'хлеб, ломтик': 1,
+        'колбаса, ломтик': 1,
+        'сыр, ломтик': 1,
+        'помидор, ломтик': 1,
+    },
+}
+
+
+def index(request):
+    return HttpResponse('Такого рецепта не знаю :(')
+
+
+def recipes(request, a):
+    servings = int(request.GET.get("servings", 1))
+    recipe = {}
+    if servings:
+        for key, value in DATA[a].items():
+            recipe[key] = value * servings
+    context = {
+        'recipe': recipe,
+    }
+    return render(request, 'calculator/index.html', context)
